6022acc9a4922dcab77bd5bc27b4906d3af54938
Meldrum, Nick <nick.meldrum@trowprice.com>
2024-12-02 11:18:49 +0000

    naive day1

--- day1/index.mjs
+++ day1/index.mjs
@@ -1,6 +1,39 @@
 import * as utils from '../utils/index.mjs'
 
 utils.run(import.meta.url, (day, input, lines) => {
-    const linesSplit = utils.parseSpacedToArrayInAllLines(lines)
-    console.log(`day ${day}. oh hai, ${linesSplit[999][3]}`)
+  const linesSplit = utils.parseSpacedToArrayInAllLines(lines)
+  const left = linesSplit.map(l => l[0])
+  const right = linesSplit.map(l => l[3])
+
+  // part 1
+  ////////////////////////
+  //
+  left.sort((a, b) => a - b)
+  right.sort((a, b) => a - b)
+
+  let cumulativeDistance = 0
+
+  for(let i = 0; i < left.length; i += 1) {
+    cumulativeDistance += Math.abs(left[i] - right[i])
+  }
+
+  console.log(`total distance: ${cumulativeDistance}`)
+
+
+  // part 2
+  ////////////////////////
+
+  let cumulativeSum = 0
+
+  for(let i = 0; i < left.length; i += 1) {
+    let times = 0
+    for(let j = 0; j < right.length; j += 1) {
+      if (right[j] === left[i]) {
+        times += 1
+      }
+    }
+    cumulativeSum += left[i] * times
+  }
+
+  console.log(`total sum: ${cumulativeSum}`)
 })
